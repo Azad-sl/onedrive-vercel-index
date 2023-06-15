7599975dbaa6117a9e5043ee8b3fcee9e38d0e05
Azad-sl <85327306+Azad-sl@users.noreply.github.com>
2023-06-15 23:50:09 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -7,20 +7,20 @@ module.exports = {
   // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
   // You can also put this in your Vercel's environment variable 'NEXT_PUBLIC_USER_PRINCIPLE_NAME' if you worry about
   // your email being exposed in public.
-  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'spencer@spwoo.onmicrosoft.com',
+  userPrincipalName: process.env。NEXT_PUBLIC_USER_PRINCIPLE_NAME || '201611060116@stu.scau.edu.cn',
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
   icon: '/icons/128.png',
 
   // Prefix for KV Storage
-  kvPrefix: process.env.KV_PREFIX || '',
+  kvPrefix: process.env。KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Spencer's OneDrive",
+  标题: "Azad's OneDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/Public',
+  baseDirectory: '/挂载盘',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
@@ -38,22 +38,22 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by SpencerWoo.',
+    '<a href="http://www.azad.asia/" target="_blank" class="css-o7lu8j" title="主页">🚪 Home</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://blog.azad.asia/" target="_blank" title="博客">📖 Blog</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://ss.azad.asia/"  target="_blank" title="导航">🌐 SoHa</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://faka.aihub.ren/" target="_blank" title="购物">🛒 Shop</a><br>&Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>.&nbsp;<a href="http://www.azad.asia/dashang/" target="_blank" title="赏个铜板~">☕ 请喝咖啡</a></p></font>',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
   protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:spencer.wushangbo@gmail.com',
+  email: 'mailto:Azad_sl@88.com',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      名字: 'Alist',
+      link: 'https://alist.azad.asia',
     },
   ],
 
